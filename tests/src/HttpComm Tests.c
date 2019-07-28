ffb0088b746ab4fb58a5ac5111e638e88ead692e
Jack Mcslay <jackmcslay@hotmail.com>
2019-07-28 15:15:08 -0300

    First commit

---
+++ tests/src/HttpComm Tests.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HttpComm.c
+ Author      : Jack Mcslay
+ Version     :
+ Copyright   : BSD 3-clause Licence
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
